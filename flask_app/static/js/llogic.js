bca7cedbcda1d589d7a40c12ac1b2d0d913b56f5
RedDirtSixKiller <adam@ajpconline.com>
2019-10-15 18:03:19 -0700

    everything plots on the map, don't screw this up

--- flask_app/static/js/llogic.js
+++ flask_app/static/js/llogic.js
@@ -1,20 +1,37 @@
-var points = [{
-    "latitud": 43.526523590087891,
-    "longitud": -5.6150951385498047
-}, {
-    "latitud": 43.511680603027344,
-    "longitud": -5.6671133041381836
-},
-// 
-// More Lat-Lng Points
-// 
-{
-    "latitud": 43.526451110839844,
-    "longitud": -5.6140098571777344
-}];
+var map = L.map('map',{
+}).setView([38.51, -121.55], 12);
 
-for (var i in points) {
-var latlng = L.latLng({ lat: points[i].latitud, lng: points[i].longitud });
 
-L.marker( latlng ).addTo(map);
-}
+var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(map);
+
+var queryUrl = 'http://localhost:5000/data'
+
+d3.json(queryUrl, function(data) {
+    // Once we get a response, db need to be converted to geojson
+    var jsonFeatures = [];
+  
+  data.forEach(function(point){
+      var lat = point[12];
+      var lon = point[13];
+  
+      var feature = {type: 'Feature',
+          properties: point,
+          geometry: {
+              type: 'Point',
+              coordinates: [lon,lat]
+          }
+      };
+
+      jsonFeatures.push(feature);
+  })
+
+
+  var geoJson = { type: 'FeatureCollection', features: jsonFeatures };
+  console.log(geoJson)
+  L.geoJson(geoJson).addTo(map);
+});
